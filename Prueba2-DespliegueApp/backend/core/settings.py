b7aab536633fa9f139a8fecbfec5cb8453d5d896
LucianoLoyola <lucianonicolasloyola@alu.ing.unlp.edu.ar>
2025-02-02 17:20:14 -0300

    Testeando con Serve

--- Prueba2-DespliegueApp/backend/core/settings.py
+++ Prueba2-DespliegueApp/backend/core/settings.py
@@ -153,8 +153,11 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost",
     "http://localhost:80",
+    "http://localhost:3000",
     "http://127.0.0.1",
-    "http://127.0.0.1:80"
+    "http://127.0.0.1:80",
+    "http://127.0.0.1:3000"
+    
 ]
 
 # ##################################################################### #
